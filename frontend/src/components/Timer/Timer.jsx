1494baa765ff47a8204ab959656a558ab4171564
ethandoes-code <ethandoescode@gmail.com>
2024-12-29 10:18:01 -0600

    Change timer layout

--- frontend/src/components/Timer/Timer.jsx
+++ frontend/src/components/Timer/Timer.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import "./Timer.css";
 
 function Timer() {
-    let now = new Date();
-
-    const [time, setTime] = useState(setTime);
-
-    function handleTime() {
-        const minutes = now.getMinutes().toString().padStart(2, "0");
-        const seconds = now.getSeconds().toString().padStart(2, "0");
-        const timeStr = `${minutes}:${seconds}`;
-        setTime(timeStr);
-    }
-
     return (
         <>
             <div className="timer-container">
                 <div>Focus</div>
-                <label className="time">{time}</label>
+                <label className="time">00:00</label>
                 <div className="btn-container">
                     <button id="play-btn">Play</button>
                     <button id="pause-btn">Pause</button>
